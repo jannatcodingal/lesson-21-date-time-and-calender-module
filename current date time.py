0b396335d7c089a7f2d3bb7bf72fff8e63a52652
jannatcodingal <jannatrehman007@gmail.com>
2025-05-04 19:25:59 +0500

    date and time

---
+++ current date time.py
@@ -0,0 +1,6 @@
+from datetime import date , time , datetime
+today=date.today()
+now=datetime.now()
+print("todays date is",today)
+print("todays date and time is",now)
+print("date components are",today.year,today.month,today.day)
